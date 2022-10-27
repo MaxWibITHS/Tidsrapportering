4798bf6558e669929169759518b654f08bd48d52
mawib <mawib@DESKTOP-KDO6U7N>
2022-10-27 16:48:40 +0200

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tidsrapportering
+{
+    internal class ArbetsInfo
+    {
+       public string Name { get; set; }
+       public string Date { get; set; }
+       public string WorkStart {get; set;}  
+       public string WorkEnd {get; set;} 
+       public string WorkType { get; set; }
+        public string Comment { get; set; }
+
+        public ArbetsInfo(string name, string date, string workStart, string workEnd, string workType, string comment)
+        {
+            this.Name = name;
+            this.Date = date;
+            this.WorkStart = workStart;
+            this.WorkEnd = workEnd;
+            this.WorkType = workType;
+            this.Comment = comment;
+        }   
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tidsrapportering
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<ArbetsInfo> minLista = new List<ArbetsInfo>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void myButton_Click(object sender, RoutedEventArgs e)
+        {
+            string namn = nameBox.Text;
+            string datum = dateBox.Text;
+            string start = ArbeteStartBox.Text;
+            string end = ArbeteAvslutBox.Text;
+            string arbete = WorkBox.Text;
+            string kommentar = CommentBox.Text;
+
+            ArbetsInfo nyttArbete = new ArbetsInfo(namn, datum, start, end, arbete, kommentar);
+            minLista.Add(nyttArbete);
+
+            
+            MyListBox.Items.Add(nyttArbete.Date + " kl. " + nyttArbete.WorkStart + "-" + nyttArbete.WorkEnd + " Uppdrag: " + nyttArbete.WorkType + " Namn: " + nyttArbete.Name);    
+            
+        }
+
+        private void MyListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+    }
+
